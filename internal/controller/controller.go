c5a88e1deec0e2ebb28e9ead822bef6418898b1f
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2023-12-17 14:12:18 +0700

    feat: even more boilerplates

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -7,6 +7,12 @@ import (
 
 type IController interface {
 	HealthCheck(ctx echo.Context) error
+	GetAllFoods(ctx echo.Context) error
+	GetFoodDetail(ctx echo.Context) error
+	GetAllShops(ctx echo.Context) error
+	GetShopDetail(ctx echo.Context) error
+	GetUserHistory(ctx echo.Context) error
+	DetectFood(ctx echo.Context) error
 }
 
 type controller struct {
@@ -17,5 +23,31 @@ func New() IController {
 }
 
 func (c *controller) HealthCheck(ctx echo.Context) error {
+	return ctx.JSON(200, util.MakeResponse(200, "OK", nil, nil))
+}
+
+// TODO: Implement all functions below
+
+func (c *controller) GetAllFoods(ctx echo.Context) error {
+	return ctx.JSON(200, util.MakeResponse(200, "OK", nil, nil))
+}
+
+func (c *controller) GetFoodDetail(ctx echo.Context) error {
+	return ctx.JSON(200, util.MakeResponse(200, "OK", nil, nil))
+}
+
+func (c *controller) GetAllShops(ctx echo.Context) error {
+	return ctx.JSON(200, util.MakeResponse(200, "OK", nil, nil))
+}
+
+func (c *controller) GetShopDetail(ctx echo.Context) error {
+	return ctx.JSON(200, util.MakeResponse(200, "OK", nil, nil))
+}
+
+func (c *controller) GetUserHistory(ctx echo.Context) error {
+	return ctx.JSON(200, util.MakeResponse(200, "OK", nil, nil))
+}
+
+func (c *controller) DetectFood(ctx echo.Context) error {
 	return ctx.JSON(200, util.MakeResponse(200, "OK", nil, nil))
 }
--- internal/routes/main.go
+++ internal/routes/main.go
@@ -13,4 +13,22 @@ func InitRoutes(e *echo.Echo) {
 	v1 := e.Group("/api/v1")
 
 	v1.GET("/health", c.HealthCheck)
+
+	// Food
+	food := v1.Group("/food")
+	food.GET("", c.GetAllFoods)
+	food.GET("/:id", c.GetFoodDetail)
+
+	// Shop
+	shop := v1.Group("/shop")
+	shop.GET("", c.GetAllShops)
+	shop.GET("/:id", c.GetShopDetail)
+
+	// User
+	user := v1.Group("/user")
+	user.GET("/:id/history", c.GetUserHistory)
+
+	// Detect
+	detect := v1.Group("/detect")
+	detect.POST("", c.DetectFood)
 }
